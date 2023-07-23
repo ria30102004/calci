40d92e26ecbcfa60456e0facff37c450da5ded03
ria30102004 <138691860+ria30102004@users.noreply.github.com>
2023-07-23 13:04:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log('Button text is ',buttonText);
+        if(buttonText=='C'){
+            screen.value="";
+        }
+        else if(buttonText=='='){
+            screen.value=eval(screenValue);
+
+        }
+        else{
+            screenValue +=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
